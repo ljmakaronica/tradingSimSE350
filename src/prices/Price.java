48cb5fc1f4209a3072b2d93c3a2214bf0f527e83
ljmakaronica <makaronica16@gmail.com>
2025-01-25 15:30:25 -0600

    projecr done.

--- src/prices/Price.java
+++ src/prices/Price.java
@@ -1,6 +1,7 @@
 package prices;
+import java.util.Objects;
 
-public class Price
+public class Price implements Comparable<Price>
 {
     private final int cents;
 
@@ -9,9 +10,89 @@ public class Price
         this.cents = cents;
     }
 
-    public Price add(Price p2)
+    public boolean isNegative()
     {
-        return PriceFactory.makePrice(cents + p2.cents);
+        return cents < 0;
+    }
+
+    public Price add(Price p) throws InvalidPriceException
+    {
+        if (p == null) {
+            throw new InvalidPriceException("Price cannot be null");
+        }
+        return PriceFactory.makePrice(cents + p.cents);
+    }
+
+    public Price subtract(Price p) throws InvalidPriceException
+    {
+        if (p == null) {
+            throw new InvalidPriceException("Price cannot be null");
+        }
+        return PriceFactory.makePrice(cents - p.cents);
+    }
+
+    public Price multiply(int n)
+    {
+        return PriceFactory.makePrice(cents * n);
+    }
+
+    public boolean greaterOrEqual(Price p) throws InvalidPriceException
+    {
+        if (p == null) {
+            throw new InvalidPriceException("Price cannot be null");
+        }
+        return cents >= p.cents;
+    }
+    public boolean lessOrEqual(Price p) throws InvalidPriceException
+    {
+        if (p == null) {
+            throw new InvalidPriceException("Price cannot be null");
+        }
+        return cents <= p.cents;
+    }
+    public boolean greaterThan(Price p) throws InvalidPriceException
+    {
+        if (p == null) {
+            throw new InvalidPriceException("Price cannot be null");
+        }
+        return cents > p.cents;
+    }
+    public boolean lessThan(Price p) throws InvalidPriceException
+    {
+        if (p == null) {
+            throw new InvalidPriceException("Price cannot be null");
+        }
+        return cents < p.cents;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o == null || getClass() != o.getClass()) return false;
+        Price price = (Price) o;
+        return cents == price.cents;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hashCode(cents);
+    }
+
+    @Override
+    public int compareTo(Price p)
+    {
+        if (p == null)
+        {
+            return -1;
+        }
+        return Math.abs(cents - p.cents);
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("$%,.2f", cents/100.0);
     }
 
 }
--- src/prices/PriceFactory.java
+++ src/prices/PriceFactory.java
@@ -1,5 +1,75 @@
 package prices;
-
-public class PriceFactory
+import java.util.Objects;
+public abstract class PriceFactory
 {
+    public static Price makePrice (int value)
+    {
+        return new Price(value);
+    }
+    public static Price makePrice (String stringValueIn) throws InvalidPriceException
+    {
+        if (stringValueIn == null || stringValueIn.isEmpty())
+        {
+            throw new InvalidPriceException("Price format is invalid");
+        }
+        String inputPrice = stringValueIn;
+        for (int i = 0; i < inputPrice.length(); i++) {
+            char c = inputPrice.charAt(i);
+            if (!Character.isDigit(c) && c != '$' && c != '.' && c != '-' && c != ',') {
+                throw new InvalidPriceException("Price format is invalid");
+            }
+        }
+        if (inputPrice.contains("$") && inputPrice.indexOf("$") != 0) {
+            throw new InvalidPriceException("Price format is invalid");
+        }
+
+        int numberOfDecimals = 0;
+        for (int i = 0; i < inputPrice.length(); i++)
+        {
+            char c = inputPrice.charAt(i);
+            if (c == '.')
+            {
+                numberOfDecimals++;
+            }
+        }
+        if(numberOfDecimals > 1)
+        {
+            throw new InvalidPriceException("Price format is invalid.");
+
+        }
+        int numbersAfterDecimal = 0;
+        boolean decimalPresent = false;
+        for (int i = 0; i < inputPrice.length(); i++)
+        {
+            char c = inputPrice.charAt(i);
+
+            if (c == '.')
+            {
+                decimalPresent = true;
+            }
+            else if (decimalPresent && Character.isDigit(c))
+            {
+                numbersAfterDecimal++;
+            }
+
+        }
+        if (numbersAfterDecimal == 1 || numbersAfterDecimal > 2)
+        {
+            throw new InvalidPriceException("Price format is invalid.");
+        }
+        String allCleanedUp = inputPrice.replaceAll("\\$","").replaceAll(",","");
+
+        if(decimalPresent && numbersAfterDecimal > 0)
+        {
+            allCleanedUp = allCleanedUp.replaceAll("\\.","");
+        }
+        else
+        {
+            allCleanedUp = allCleanedUp.replaceAll("\\.","") + "00";
+        }
+
+        int newValue = Integer.parseInt(allCleanedUp);
+        return makePrice(newValue);
+    }
+
 }
